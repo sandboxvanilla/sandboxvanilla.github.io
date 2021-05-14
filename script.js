e8c9cfeca0a052fa9d35e4bd9071a143754e41f1
gustavs-vagalis <gustavs.vagalis@gmail.com>
2021-05-14 09:55:05 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var images = ["1.png", "2.png", "4.png", "5.jpg", "7.png"];
+var prefix = "url(\'./backgrounds/";
+var suffix = "\')";
+
+function randomBg() {
+  const random = Math.floor(Math.random() * images.length);
+  console.log(prefix.concat(images[random]), suffix);
+  document.body.style.backgroundImage = prefix.concat(images[random], suffix);
+  document.body.style.backgroundAttachment = "fixed";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "repeat-y";
+  document.body.style.backgroundSize = "cover";
+
+}
